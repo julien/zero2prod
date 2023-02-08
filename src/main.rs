868d81bc22aed4a54ffa9053f432811240ff6a4b
Julien Castelain <julien@users.noreply.github.com>
2023-02-08 22:02:25 +0100

    Change error message+remove useless comment

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,11 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
-        .expect("failed to connect to sqlite");
+        .expect("failed to connect to postgres");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our server
+
     run(listener, connection_pool)?.await;
     Ok(())
 }
