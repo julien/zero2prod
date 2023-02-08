211faf4050143d0d228755a20946f0b856e6207d
Julien Castelain <julien@users.noreply.github.com>
2023-02-08 22:03:07 +0100

    More code in the right place

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -72,9 +72,9 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
