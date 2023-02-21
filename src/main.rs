02002b55b6593c4688d55e58e5336320f78ada36
Julien Castelain <julien@users.noreply.github.com>
2023-02-21 23:54:04 +0100

    Configure logger

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
