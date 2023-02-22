96c57b0424c517bf7f859e617bd2b91785bdfe2c
Julien Castelain <julien@users.noreply.github.com>
2023-02-22 23:23:19 +0100

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-
 use secrecy::ExposeSecret;
-
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
