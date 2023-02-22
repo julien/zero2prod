a456fcec478d4da22cc0284871ba805e41a9eef6
Julien Castelain <julien@users.noreply.github.com>
2023-02-22 20:57:11 +0100

    Replace log with tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,13 +11,13 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
+    tracing::info!(
         "request_id {} - adding '{}' '{}' as a new subscriber",
         request_id,
         form.email,
         form.name
     );
-    log::info!(
+    tracing::info!(
         "request_id {} - saving new subscriber details in the database",
         request_id
     );
@@ -35,14 +35,14 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request_id {} - new subscriber details have been saved",
                 request_id
             );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "request_id {} - failed to execute query: {:?}",
                 request_id,
                 e
