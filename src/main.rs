51ebbd3520d6aa5494591581c224088311b65dd8
Julien Castelain <julien@users.noreply.github.com>
2023-03-01 21:56:04 +0100

    Implement email_client + simple test

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,12 @@ async fn main() -> Result<(), std::io::Error> {
         .email_client
         .sender()
         .expect("invalid sender email address");
-    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
 
     let address = format!(
         "{}:{}",
