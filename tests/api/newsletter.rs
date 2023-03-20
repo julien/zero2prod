1c82017b9522ef7906f5c908931c9dbb792e3e25
Julien Castelain <julien@users.noreply.github.com>
2023-03-20 21:12:09 +0100

    Define types for newsletter body

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -108,3 +108,41 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
     assert_eq!(response.status().as_u16(), 200);
     // Mock verifies on Drop that we have sent the newsletter email
 }
+
+#[tokio::test]
+async fn newsletters_returns_400_for_invalid_data() {
+    // Arrange
+    let app = spawn_app().await;
+    let test_cases = vec![
+        (
+            serde_json::json!({
+                "content": {
+                    "text": "Newsletter body as plain text",
+                    "html": "<p>Newsletter body as HTML</p>",
+                }
+            }),
+            "missing title",
+        ),
+        (
+            serde_json::json!({"title": "Newsletter!"}),
+            "missing content",
+        ),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = reqwest::Client::new()
+            .post(&format!("{}/newsletters", &app.address))
+            .json(&invalid_body)
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        // Ass
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "the API did not fail with 400 Bad Request when payload was {}",
+            error_message,
+        );
+    }
+}
