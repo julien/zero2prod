69e040e5e3488e1df1d4c60bfd48be1a495dadf2
Julien Castelain <julien@users.noreply.github.com>
2023-03-21 21:14:56 +0100

    Implement Display for SubscriberEmail

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -19,6 +19,14 @@ impl AsRef<str> for SubscriberEmail {
     }
 }
 
+impl std::fmt::Display for SubscriberEmail {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Forward to the Display implementation of
+        // the wrapped string.
+        self.0.fmt(f)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
