b63c16179c2badae2aa51739d3df215512ddee5d
Julien Castelain <julien@users.noreply.github.com>
2023-03-27 21:18:33 +0200

    Verify password in another thread

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -66,20 +66,38 @@ async fn validate_credentials(
         .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("unknown username")))?;
 
-    let expected_password_hash = PasswordHash::new(&expected_password_hash.expose_secret())
-        .map_err(|_| PublishError::AuthError(anyhow::anyhow!("expected error")))?;
+    // let expected_password_hash = PasswordHash::new(&expected_password_hash.expose_secret())
+    //     .map_err(|_| PublishError::AuthError(anyhow::anyhow!("expected error")))?;
 
-    tracing::info_span!("verify password hash")
-        .in_scope(|| {
-            Argon2::default().verify_password(
-                credentials.password.expose_secret().as_bytes(),
-                &expected_password_hash,
-            )
-        })
-        .context("invalid password")
-        .map_err(PublishError::AuthError)?;
+    tokio::task::spawn_blocking(move || {
+        verify_password_hash(expected_password_hash, credentials.password)
+    })
+    .await
+    .context("failed to spawn blocking task")
+    .map_err(PublishError::UnexpectedError)??;
 
     Ok(user_id)
+}
+
+#[tracing::instrument(
+    name = "verify password hash",
+    skip(expected_password_hash, password_candidate)
+)]
+fn verify_password_hash(
+    expected_password_hash: Secret<String>,
+    password_candidate: Secret<String>,
+) -> Result<(), PublishError> {
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
+        .context("failed to parse hash in PHC string format")
+        .map_err(PublishError::UnexpectedError)?;
+
+    Argon2::default()
+        .verify_password(
+            password_candidate.expose_secret().as_bytes(),
+            &expected_password_hash,
+        )
+        .context("invalid password")
+        .map_err(PublishError::AuthError)
 }
 
 #[tracing::instrument(name = "get stored credentials", skip(username, pool))]
