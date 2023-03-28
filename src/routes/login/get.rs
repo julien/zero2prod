049531b688d47194704a2eee8c2c566af1c426da
Julien Castelain <julien@users.noreply.github.com>
2023-03-28 22:00:01 +0200

    Add more test for login feature

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,65 +1,13 @@
-use crate::startup::HmacSecret;
-use actix_web::{http::header::ContentType, web, HttpResponse};
-use hmac::{Hmac, Mac};
-use secrecy::ExposeSecret;
+use actix_web::{http::header::ContentType, HttpRequest, HttpResponse};
 
-#[derive(serde::Deserialize)]
-pub struct QueryParams {
-    error: String,
-    tag: String,
-}
-
-impl QueryParams {
-    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
-        let tag = hex::decode(self.tag)?;
-        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error),);
-
-        let mut mac =
-            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
-        mac.update(query_string.as_bytes());
-        mac.verify_slice(&tag)?;
-
-        Ok(self.error)
-    }
-}
-
-pub async fn login_form(
-    query: Option<web::Query<QueryParams>>,
-    secret: web::Data<HmacSecret>,
-) -> HttpResponse {
-    let error_html = match query {
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
         None => "".into(),
-        Some(query) => match query.0.verify(&secret) {
-            Ok(error) => {
-                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
-            }
-            Err(e) => {
-                tracing::warn!(
-                    error.message = %e,
-                    error.cause_chain = ?e,
-                    "failed to verify query parameters using the HMAC tag",
-                );
-                "".into()
-            }
-        },
+        Some(cookie) => {
+            format!("<p><i>{}</i></p>", cookie.value())
+        }
     };
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(format!(
-            r#"<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset="utf-8">
-    <title>Login</title>
-</head>
-<body>
-    {error_html}
-    <form action="/login" method="post">
-        <label>Username <input type="text" placeholder="Enter username" name="username"></label>
-        <label>Password <input type="password" placeholder="Enter password" name="password"></label>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html>"#,
-        ))
+        .body(format!("{}", error_html))
 }
