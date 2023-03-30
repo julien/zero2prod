f478cf7c8137428b8616343dce85143c79de3428
Julien Castelain <julien@users.noreply.github.com>
2023-03-30 09:44:31 +0200

    Add a typed interface to session

--- src/session_state.rs
+++ src/session_state.rs
@@ -1,4 +1,7 @@
-use actix_session::{Session, SessionGetError, SessionInsertError};
+use actix_session::{Session, SessionExt, SessionGetError, SessionInsertError};
+use actix_web::dev::Payload;
+use actix_web::{FromRequest, HttpRequest};
+use std::future::{ready, Ready};
 use uuid::Uuid;
 
 pub struct TypedSession(Session);
@@ -16,5 +19,21 @@ impl TypedSession {
 
     pub fn get_user_id(&self) -> Result<Option<Uuid>, SessionGetError> {
         self.0.get(Self::USER_ID_KEY)
+    }
+}
+
+impl FromRequest for TypedSession {
+    // This is a complicated way of saying
+    // Return the same error returned by the implementation
+    // of FromRequest for Session
+    type Error = <Session as FromRequest>::Error;
+
+    // Rust doesn't support async syntax in traits
+    // FromRequest expects a Future as the return type to allow
+    // for extractors that need to perform async operations
+    type Future = Ready<Result<TypedSession, Self::Error>>;
+
+    fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
+        ready(Ok(TypedSession(req.get_session())))
     }
 }
