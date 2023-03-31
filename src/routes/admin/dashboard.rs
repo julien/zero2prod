d0b2b0b5b84784e4e8e81d93043eeb3e8bddb68d
Julien Castelain <julien@users.noreply.github.com>
2023-03-31 20:42:05 +0200

    Finish change password

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,17 +1,10 @@
 use crate::session_state::TypedSession;
+use crate::utils::e500;
 use actix_web::http::header::{ContentType, LOCATION};
 use actix_web::{web, HttpResponse};
 use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
-
-// Returns an opaque 500 error while preserving the error's root cause for logging.
-fn e500<T>(e: T) -> actix_web::Error
-where
-    T: std::fmt::Debug + std::fmt::Display + 'static,
-{
-    actix_web::error::ErrorInternalServerError(e)
-}
 
 pub async fn admin_dashboard(
     session: TypedSession,
@@ -35,6 +28,10 @@ pub async fn admin_dashboard(
 </head>
 <body>
     <p>Welcome {username}!</p>
+    <p>Available actions:</p>
+    <ol>
+        <li><a href="/admin/password">Change password</a></li>
+    </ol>
 </body>
 </html>"#
         )))
--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,14 +1,17 @@
+use crate::session_state::TypedSession;
 use actix_web::{http::header::ContentType, HttpResponse};
 use actix_web_flash_messages::{IncomingFlashMessages, Level};
 use std::fmt::Write;
 
-pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
+pub async fn login_form(
+    flash_messages: IncomingFlashMessages,
+) -> Result<HttpResponse, actix_web::Error> {
     let mut error_html = String::new();
     for m in flash_messages.iter().filter(|m| m.level() == Level::Error) {
         writeln!(error_html, "<p><i>{}</i></p>", m.content()).unwrap();
     }
 
-    HttpResponse::Ok()
+    Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
@@ -38,5 +41,5 @@ pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
     </form>
 </body>
 </html>"#,
-        ))
+        )))
 }
